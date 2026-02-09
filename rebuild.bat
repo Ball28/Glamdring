64c2cf9d4ba0dbc5dc89c235b6e5385b891fa734
Evan <balleva28@gmail.com>
2026-02-09 14:58:47 -0600

    Update rebuild.bat

--- rebuild.bat
+++ rebuild.bat
@@ -2,7 +2,7 @@
 echo ===================================================
 echo   Rebuilding Glamdring.exe... 🔨
 echo ===================================================
-
+pip install pyInstaller
 pip install -r requirements.txt 
 
 python build_exe.py
